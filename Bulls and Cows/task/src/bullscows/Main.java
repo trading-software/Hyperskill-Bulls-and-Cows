03e8f84a95184311b2e531b23154596698373cd8
Pawel <pavvel100@gmail.com>
2020-10-08 19:54:12 +0200

    Completed stage 2 of the Project: Grader.

---
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -0,0 +1,80 @@
+package bullscows;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] predefinedCode = createCode();
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+//        int[] answer = stringToCode(s);
+        int[] grading = grader(s, predefinedCode);
+
+        int bulls = grading[0];
+        int cows = grading[1];
+
+        if (bulls == 0 && cows == 0) {
+            System.out.printf("\nGrade: None. The secret code is %s.", codeToString(predefinedCode));
+        } else if (bulls == 0) {
+            System.out.printf("\nGrade: %s cow(s). The secret code is %s.", cows, codeToString(predefinedCode));
+        } else {
+            System.out.printf("\nGrade: %s bull(s) and %s cow(s). The secret code is %s.", bulls, cows,
+                    codeToString(predefinedCode));
+        }
+
+
+
+
+    }
+    public static int[] stringToCode(String str) {
+        int[] code = new int[str.length()];
+        for (int i = 0; i < str.length(); i++) {
+            code[i] = Integer.parseInt("" + str.charAt(i));
+        }
+        return code;
+    }
+    public static String codeToString(int[] code) {
+        String s = "";
+        for (int i = 0; i < code.length; i++) {
+            s+= code[i];
+        }
+
+        return s;
+    }
+
+    public static int[] createCode() {
+        Random random = new Random();
+        int[] code = new int[4];
+        for (int i = 0; i < code.length; i++) {
+            code[i] = random.nextInt(10);
+        }
+        return code;
+    }
+
+    public static int[] grader(String answerStr, int[] code) {
+        int bulls = 0;
+        int cows = 0;
+        int[] answer = new int[answerStr.length()];
+        for (int i = 0; i < answerStr.length(); i++) {
+            answer[i] = Integer.parseInt("" + answerStr.charAt(i));
+        }
+
+        for (int i = 0; i < code.length; i++) {
+            if (answer[i] == code[i]) {
+                bulls++;
+            } else if (contains(answer[i], code)) {
+                cows++;
+            }
+        }
+        return new int[]{bulls, cows};
+    }
+
+    public static boolean contains(int x, int[] code) {
+        for (int i = 0; i < code.length; i++) {
+            if (code[i] == x)
+                return true;
+        }
+        return false;
+    }
+}
